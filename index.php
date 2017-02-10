fa3a59a7dafea33083defde7e2e85d607ac98582
Ghash <neoandrew@yandex.ru>
2017-02-10 21:10:27 +0300

    Added new pages. Added basic backgrounds.

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en">
 <head>
 <!--
--подключение шрифтов
--базовое форматирование
--базовые заливки и фоны
++подключение шрифтов
++базовое форматирование
++базовые заливки и фоны
 -меню переключения
 -svg
 -текст
@@ -26,8 +26,8 @@
 	<script>
 	jQuery(document).ready(function($) {
 		$("#fullpage").fullpage({
-			sectionsColor:['#ffe6e6','#e6ffff','#ecffe6'],
-			anchors:['fistPage','secondPage','3rdPage'],
+			sectionsColor:['white','#5c717c','white','#ffea00','white','#484848'],
+			anchors:['fistPage','secondPage','3rdPage','4thPage','5thPage','6thPage'],
 			menu:'#menu',
 			scrollBar:false,
 			//scrollOverflow:true,
@@ -43,6 +43,9 @@
 		<li data-menuanchor="fistPage"><a href="#fistPage">fistSlaider</a></li>
 		<li data-menuanchor="secondPage"><a href="#secondPage">secondSlider</a></li>
 		<li data-menuanchor="3rdPage"><a href="#3rdPage">3rd Slider</a></li>
+		<li data-menuanchor="4thPage"><a href="#4thPage">4th Slider</a></li>
+		<li data-menuanchor="5thPage"><a href="#5thPage">5th Slider</a></li>
+		<li data-menuanchor="6thPage"><a href="#6thPage">6th Slider</a></li>
 	</ul>
 <div id="fullpage">
 	<div class="section" id="section1">
@@ -60,6 +63,15 @@
 	<div class="section" id="section3">
 		<p>Quisquam ex possimus, quia tenetur. Rerum facere nesciunt esse officia sit minima quidem laboriosam nobis, fugit laudantium. Molestias placeat facere, laudantium quidem voluptatibus voluptatum nobis libero commodi, iure tempora ducimus.</p>
 	</div>
+	<div class="section" id="section4">
+		<p>Explicabo nemo quae animi natus cum porro in sequi perspiciatis vel veritatis. Quaerat laborum repudiandae, ratione ipsa similique. Nihil obcaecati, consequuntur incidunt qui assumenda laudantium eius aut. Suscipit, tempore corporis.</p>
+	</div>
+	<div class="section" id="section5">
+		<p>Quisquam ex possimus, quia tenetur. Rerum facere nesciunt esse officia sit minima quidem laboriosam nobis, fugit laudantium. Molestias placeat facere, laudantium quidem voluptatibus voluptatum nobis libero commodi, iure tempora ducimus.</p>
+	</div>	
+	<div class="section" id="section6">
+		<p>Quisquam ex possimus, quia tenetur. Rerum facere nesciunt esse officia sit minima quidem laboriosam nobis, fugit laudantium. Molestias placeat facere, laudantium quidem voluptatibus voluptatum nobis libero commodi, iure tempora ducimus.</p>
+	</div>		
 </div>
 	
 </body>
